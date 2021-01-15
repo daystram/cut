7d3ba01e885e763ff38eab19f9fa09148e8f57cc
Danny August Ramaputra <daystram@gmail.com>
2021-01-15 15:03:22 +1000

    Added Home page and mock Create page

--- cut-fe/src/views/Home.vue
+++ cut-fe/src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-container fill-height fluid class="gradient-bg">
+    <v-col>
+      <v-row>
+        <v-col>
+          <h1 class="text-sm-h1 text-h3 text-center">
+            <Logo />
+          </h1>
+          <p
+            class="mt-12 mb-12 text-subtitle-1 ma-auto text-center app-subtitle"
+          >
+            URL, snippet, and file sharing with ease.
+          </p>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-btn
+          :to="{ name: 'login' }"
+          elevation="6"
+          rounded
+          x-large
+          class="ma-2"
+          v-text="'Sign In'"
+          color="primary darken-2"
+        />
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-btn
+          href="https://github.com/daystram/cut"
+          x-large
+          class="mt-12"
+          text
+          plain
+          rounded
+        >
+          View on GitHub <v-icon class="ml-1" v-text="'mdi-github'" />
+        </v-btn>
+      </v-row>
+    </v-col>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from "vue";
+import Logo from "@/components/Logo.vue";
+import { authManager } from "@/auth";
 
-@Component({
-  components: {
-    HelloWorld
+export default Vue.extend({
+  components: { Logo },
+
+  computed: {
+    isAuthenticated() {
+      return authManager.isAuthenticated();
+    }
   }
-})
-export default class Home extends Vue {}
+});
 </script>
---
+++ cut-fe/src/views/manage/Create.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="create">
+    <v-row class="mb-8" align="center">
+      <v-col cols="12" sm="">
+        <h1 class="text-h2">
+          Cuts
+        </h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        MOCK CREATE
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { authManager } from "@/auth";
+
+export default Vue.extend({
+  computed: {
+    user: () => authManager.getUser()
+  }
+});
+</script>
