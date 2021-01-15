77c2f00dfffba91191408b9ea90b8b0228eb6ed1
Danny August Ramaputra <daystram@gmail.com>
2021-01-15 15:02:05 +1000

    Added logo

---
+++ cut-fe/src/components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <span style="user-select: none">
+      <v-icon
+        style="font-size: inherit; vertical-align: baseline"
+        v-text="'mdi-clipboard-text-multiple'"
+        color="secondary lighten-1"
+      />Cut
+    </span>
+  </div>
+</template>
