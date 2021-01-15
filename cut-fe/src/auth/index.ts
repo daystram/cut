1de356b7c576b9a1556c1bc1a839411e754f4648
Danny August Ramaputra <daystram@gmail.com>
2021-01-15 15:02:36 +1000

    Added AuthManager library and auth functions

---
+++ cut-fe/src/auth/index.ts
@@ -0,0 +1,88 @@
+import router from "@/router";
+import { AuthManager, ACCESS_TOKEN } from "@/auth/AuthManager";
+import { Route } from "vue-router";
+
+const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
+const ISSUER = process.env.VUE_APP_OAUTH_ISSUER;
+const REDIRECT_URI = `${location.origin}/callback`;
+
+const authManager = new AuthManager({
+  clientId: CLIENT_ID,
+  redirectUri: REDIRECT_URI,
+  issuer: ISSUER,
+  storage: sessionStorage
+});
+
+const login = function() {
+  authManager.authorize();
+};
+
+const logout = function() {
+  authManager.logout().then(() => {
+    router.replace({ name: "home" });
+  });
+};
+
+const callback = function() {
+  const params = new URLSearchParams(document.location.search);
+  const code = params.get("code");
+  const state = params.get("state");
+  if (!code || !state || !authManager.checkState(state)) {
+    router.replace("/");
+    return;
+  }
+  authManager
+    .redeemToken(code)
+    .then(() => {
+      const lastRoute = sessionStorage.getItem("last_route");
+      if (lastRoute) {
+        sessionStorage.removeItem("last_route");
+        router.replace({
+          path: lastRoute?.toString()
+        });
+      } else {
+        router.replace({
+          name: "manage:create"
+        });
+      }
+    })
+    .catch(() => {
+      router.replace({ name: "home" });
+    });
+};
+
+const refreshAuth = function(destinationPath: string) {
+  sessionStorage.setItem("last_route", destinationPath);
+  authManager.reset();
+  authManager.authorize(true);
+};
+
+const authenticatedOnly = function(to: Route, from: Route, next: () => void) {
+  if (authManager.getToken(ACCESS_TOKEN)) {
+    next();
+  } else {
+    refreshAuth(to.fullPath);
+  }
+};
+
+const unAuthenticatedOnly = function(
+  to: object,
+  from: object,
+  next: () => void
+) {
+  if (!authManager.getToken(ACCESS_TOKEN)) {
+    next();
+  } else {
+    router.push({ name: "manage:create" });
+  }
+};
+
+export {
+  authManager,
+  login,
+  logout,
+  callback,
+  refreshAuth,
+  authenticatedOnly,
+  unAuthenticatedOnly
+};
