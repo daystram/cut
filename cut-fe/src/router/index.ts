f3ca1345087e817a7cb00664b28462fa3da8711d
Danny August Ramaputra <daystram@gmail.com>
2021-01-15 15:04:35 +1000

    Updated router

--- cut-fe/src/router/index.ts
+++ cut-fe/src/router/index.ts
@@ -1,21 +1,73 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import {
+  authenticatedOnly,
+  callback,
+  login,
+  logout,
+  unAuthenticatedOnly
+} from "@/auth";
+import { Manage, Create } from "@/views";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
-  }
+    name: "home",
+    component: Home,
+    beforeEnter: unAuthenticatedOnly,
+    meta: {
+      title: "Cut"
+    }
+  },
+  {
+    path: "/",
+    beforeEnter: authenticatedOnly,
+    component: Manage,
+    redirect: "/create",
+    children: [
+      {
+        path: "create",
+        name: "manage:create",
+        component: Create,
+        meta: {
+          title: "Create | Cut"
+        }
+      }
+    ]
+  },
+  {
+    path: "/login",
+    name: "login",
+    beforeEnter: unAuthenticatedOnly,
+    component: login
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    beforeEnter: authenticatedOnly,
+    component: logout
+  },
+  {
+    path: "/callback",
+    name: "callback",
+    beforeEnter: unAuthenticatedOnly,
+    component: callback
+  },
+  { path: "*", redirect: { name: "home", query: {} } }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || "Cut";
+  next();
 });
 
 export default router;
