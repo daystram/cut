5cfdf107be85b1a5d034fd8df29e39001727e650
Danny August Ramaputra <daystram@gmail.com>
2021-01-16 12:44:04 +1000

    Added redis configs

--- cut-be/src/core/config.rs
+++ cut-be/src/core/config.rs
@@ -3,10 +3,14 @@ use std::env;
 #[derive(Clone)]
 pub struct AppConfig {
     pub client_secret: String,
+    pub redis_host: String,
+    pub redis_port: String,
 }
 
 pub fn init() -> AppConfig {
     return AppConfig {
         client_secret: env::var("CLIENT_SECRET").expect("CLIENT_SECRET is required"),
+        redis_host: env::var("REDIS_HOST").expect("REDIS_HOST is required"),
+        redis_port: env::var("REDIS_PORT").expect("REDIS_PORT is required"),
     };
 }
