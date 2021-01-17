9a37b683533fa0cb647ea23ab163788cb7b9b048
Danny August Ramaputra <daystram@gmail.com>
2021-01-18 07:48:17 +1000

    Added response frames

--- cut-be/src/app/datatransfers/auth.rs
+++ cut-be/src/app/datatransfers/auth.rs
@@ -3,6 +3,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TokenInfo {
     pub active: bool,
-    #[serde(default)] pub sub: String,
-    #[serde(default)] pub client_id: String,
+    #[serde(default)]
+    pub sub: String,
+    #[serde(default)]
+    pub client_id: String,
 }
--- cut-be/src/app/datatransfers/cut.rs
+++ cut-be/src/app/datatransfers/cut.rs
@@ -1,10 +1,18 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Cut {
     pub name: String,
+    #[serde(skip)]
+    pub owner: String,
+    #[serde(skip_deserializing)]
     pub variant: String,
     pub metadata: String,
     pub data: String,
     pub created_at: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateResponse {
+    pub hash: String,
 }
