b1c8bddb68d2cea0563e624d0d579a5239899f18
Danny August Ramaputra <daystram@gmail.com>
2021-01-19 16:11:30 +1000

    Implemented snippet submission

--- cut-fe/src/views/manage/Create.vue
+++ cut-fe/src/views/manage/Create.vue
@@ -29,6 +29,7 @@
                     <v-row>
                       <v-col cols="12" sm="8">
                         <v-text-field
+                          v-model.trim="snippet.name"
                           label="Name"
                           filled
                           dense
@@ -36,6 +37,11 @@
                           maxlength="50"
                           class="rounded"
                           background-color="#2d2d2d"
+                          :error-messages="snippetNameErrors"
+                          required
+                          :disabled="formLoadStatus === STATUS.LOADING"
+                          @input="$v.snippet.name.$touch()"
+                          @blur="$v.snippet.name.$touch()"
                         />
                       </v-col>
                       <v-col cols="12" sm="4">
@@ -48,6 +54,8 @@
                           background-color="#2d2d2d"
                           class="rounded"
                           style="border: none !important"
+                          required
+                          :disabled="formLoadStatus === STATUS.LOADING"
                         />
                       </v-col>
                     </v-row>
@@ -79,11 +87,91 @@
           <v-card elevation="0">
             <v-card-text class="pt-0">
               <v-col>
-                <v-btn block large color="primary darken-1">Create</v-btn>
+                <v-btn
+                  block
+                  large
+                  color="primary darken-1"
+                  :disabled="formLoadStatus === STATUS.LOADING"
+                  :loading="formLoadStatus === STATUS.LOADING"
+                  @click="create"
+                >
+                  Create
+                </v-btn>
               </v-col>
             </v-card-text>
           </v-card>
         </v-card>
+        <v-dialog
+          :value="formLoadStatus === STATUS.COMPLETE"
+          width="500"
+          persistent
+        >
+          <v-card>
+            <v-card-title>
+              <v-row no-gutters align="center">
+                <v-col cols="auto">
+                  Cut Created
+                </v-col>
+                <v-spacer />
+                <v-col cols="auto">
+                  <v-btn
+                    text
+                    icon
+                    color="grey"
+                    @click="() => (formLoadStatus = STATUS.IDLE)"
+                  >
+                    <v-icon v-text="'mdi-close'" />
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-title>
+            <v-divider inset />
+            <div class="v-card__body">
+              <!-- <v-alert
+                type="success"
+                text
+                dense
+                transition="scroll-y-transition"
+              >
+                Snippet cut created!
+              </v-alert> -->
+              <v-row align="center">
+                <v-col>
+                  <div class="mb-4">
+                    Use the following link to share your cut.
+                  </div>
+                  <div class="mt-4">
+                    <v-text-field
+                      v-model="linkView"
+                      filled
+                      dense
+                      class="rounded code-field"
+                      background-color="#2d2d2d"
+                      append-icon="mdi-clipboard-text-multiple"
+                      readonly
+                      hide-details="auto"
+                    />
+                  </div>
+                  <div class="my-4">
+                    To get view the raw cut, use the following.
+                  </div>
+                  <div class="mt-4">
+                    <v-text-field
+                      v-model="linkRaw"
+                      filled
+                      dense
+                      class="rounded code-field"
+                      background-color="#2d2d2d"
+                      append-icon="mdi-clipboard-text-multiple"
+                      readonly
+                      hide-details="auto"
+                    />
+                  </div>
+                </v-col>
+              </v-row>
+            </div>
+          </v-card>
+        </v-dialog>
       </v-col>
     </v-row>
   </div>
@@ -91,41 +179,87 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { authManager } from "@/auth";
 import { STATUS } from "@/constants/status";
 import { languages } from "@/constants/languages";
+import { maxLength, required } from "vuelidate/lib/validators";
 import "@/styles/Create.sass";
 
 import Prism from "prismjs";
 import "@/styles/prism-atom-dark.css";
+import api from "@/apis/api";
 
 export default Vue.extend({
   data() {
     return {
-      variant: null,
+      variant: 0,
       snippet: {
         name: "",
         language: "Plaintext",
         languageSelect: languages,
         data: ""
       },
-      formLoadStatus: STATUS.IDLE
+      formLoadStatus: STATUS.IDLE,
+      linkView: "",
+      linkRaw: ""
     };
   },
 
   computed: {
-    user: () => authManager.getUser()
+    snippetNameErrors() {
+      const errors: string[] = [];
+      if (!this.$v.snippet.name?.$dirty) return errors;
+      !this.$v.snippet.name.required && errors.push("Name required");
+      !this.$v.snippet.name.maxLength && errors.push("Name too long");
+      return errors;
+    }
+  },
+
+  validations: {
+    snippet: {
+      name: { required, maxLength: maxLength(50) }
+    }
   },
 
   methods: {
-    highlighter(code: string) {
+    highlighter(code: string): string {
       if (this.snippet.language === "Plaintext") return code;
       Prism.highlightAll();
       return Prism.highlight(
         code,
-        (languages as any)[this.snippet.language].grammar,
-        (languages as any)[this.snippet.language].language
+        languages[this.snippet.language].grammar,
+        languages[this.snippet.language].language
       );
+    },
+    create() {
+      switch (this.variant) {
+        case 0:
+          this.$v.snippet.$touch();
+          if (this.$v.snippet.$invalid) return;
+          this.formLoadStatus = STATUS.LOADING;
+          console.log("Creating SNIPPET");
+          api.cut
+            .create({
+              name: this.snippet.name,
+              variant: "snippet",
+              metadata: JSON.stringify({ language: this.snippet.language }),
+              data: this.snippet.data
+            })
+            .then(response => {
+              this.formLoadStatus = STATUS.COMPLETE;
+              this.snippet.name = "";
+              this.snippet.data = "";
+              this.linkView = `${window.origin}/${response.data.hash}`;
+              this.linkRaw = `${window.origin}/raw/${response.data.hash}`;
+              this.$v.snippet.$reset();
+            })
+            .catch(err => {
+              this.formLoadStatus = STATUS.IDLE;
+              console.error(err);
+            });
+          break;
+        default:
+          this.formLoadStatus = STATUS.IDLE;
+      }
     }
   }
 });
