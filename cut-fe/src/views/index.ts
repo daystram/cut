542666c6531574a80d85b8bd9fb23df99d256f8f
Danny August Ramaputra <daystram@gmail.com>
2021-01-26 15:08:57 +1000

    Added List page

--- cut-fe/src/views/index.ts
+++ cut-fe/src/views/index.ts
@@ -1,4 +1,5 @@
 export const Create = () => import("./manage/Create.vue");
 export const Home = () => import("./Home.vue");
+export const List = () => import("./manage/List.vue");
 export const Manage = () => import("./manage/Manage.vue");
 export const View = () => import("./View.vue");
